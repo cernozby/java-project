20188cf10722c98c8a36715f40bf0033d956f259
cernozby <Zbysa.Cernohous@seznam.cz>
2021-11-22 21:19:28 +0100

    init commit

--- src/main/java/cz/upol/jj/seminar08/Main.java
+++ src/main/java/cz/upol/jj/seminar08/Main.java
@@ -4,5 +4,9 @@ public class Main {
     public static void main(String[] args) {
         /* TODO code here */
         System.out.println("Hello world!");
+        System.out.println("Hello world!");
+        System.out.println("Hello world!");
     }
 }
+
+
